2e101e9197596fb5b083ead5df2e7012f4f8bd49
kunall17 <kunall.gupta17@gmail.com>
2017-04-14 20:15:22 +0530

    Added default Model table

---
+++ src/justorder/DefaultTableMOdel.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package justorder;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author kunall17
+ */
+public class DefaultTableMOdel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    public DefaultTableMOdel(String col1,String col2){
+        
+    }
+    
+    public void addRow(String name, String price){
+        
+    }
+    
+    public void removeRow(String name, String price){
+        
+    }
+    
+    
+    
+}
