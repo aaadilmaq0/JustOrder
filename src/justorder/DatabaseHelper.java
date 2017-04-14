9fdee10660be48cf9bc9bec4a4de92ff927664d6
kunall17 <kunall.gupta17@gmail.com>
2017-04-14 23:08:06 +0530

    Completed database part

--- src/justorder/DatabaseHelper.java
+++ src/justorder/DatabaseHelper.java
@@ -10,6 +10,7 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +24,7 @@ public class DatabaseHelper {
 
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/EMP";
+    static final String DB_URL = "jdbc:mysql://localhost/justorder";
     Connection conn = null;
     Statement stmt = null;
     //  Database credentials
@@ -43,7 +44,6 @@ public class DatabaseHelper {
         }
         return null;
     }
-    
 
     private DatabaseHelper() {
     }
@@ -56,32 +56,49 @@ public class DatabaseHelper {
     public List<Map.Entry<String, Integer>> getAllItems() {
         try {
             List<Map.Entry<String, Integer>> items = new ArrayList<>();
-            
+
             Connection c = getCon();
             Statement st = c.createStatement();
             ResultSet s = st.executeQuery("select * from items");
-            while(!s.next()){
+            while (!s.next()) {
                 Map.Entry<String, Integer> me = new AbstractMap.SimpleEntry<>(s.getString(0), Integer.parseInt(s.getString(1)));
                 items.add(me);
             }
-                      return items;
+            return items;
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-    
-    public void addAdmin(String name,String email,String phone, String username, String password){
-        
+
+    public void addAdmin(String name, String email, String phone, String username, String password) {
+        try {
+            StringJoiner joiner = new StringJoiner(",");
+            Connection c = getCon();
+            PreparedStatement prepareStatement = c.prepareStatement("insert into admin "
+                    + "values(" + joiner.add(username).add(password).add(name).add(email).add(phone).add(name).toString() + ");");
+            prepareStatement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public void addCustomer(String name,String email,String phone,String username,String password){
-        
+
+    public void addCustomer(String name, String email, String phone, String username, String password) {
+        try {
+            StringJoiner joiner = new StringJoiner(",");
+            Connection c = getCon();
+            PreparedStatement prepareStatement = c.prepareStatement("insert into customer "
+                    + "values(" + joiner.add(username).add(password).add(name).add(email).add(phone).add(name).toString() + ");");
+            prepareStatement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
     public void addItem(String name, double price) {
         try {
             Connection c = getCon();
-            PreparedStatement prepareStatement = c.prepareStatement("insert into items values("+name+","+price+");" );
+            PreparedStatement prepareStatement = c.prepareStatement("insert into items values(" + name + "," + price + ");");
             prepareStatement.execute();
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
@@ -89,14 +106,43 @@ public class DatabaseHelper {
 
     }
 
-    public boolean checkAdmin(String username,String password){
-        return true;
+    public boolean checkAdmin(String username, String password) {
+        try {
+            Connection c = getCon();
+            Statement st = c.createStatement();
+            ResultSet s = st.executeQuery("select username, password from admin where username=" + username + " and password=" + password);
+            s.next();
+            if (s.getString(0).equals(username) && s.getString(1).equals(password)) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            return false;
+        }
+        return false;
     }
 
-    public boolean checkCustomer(String username,String passsword){
-        return true;
+    public boolean checkCustomer(String username, String password) {
+        try {
+            Connection c = getCon();
+            Statement st = c.createStatement();
+            ResultSet s = st.executeQuery("select username, password from customer where username=" + username + " and password=" + password);
+            s.next();
+            if (s.getString(0).equals(username) && s.getString(1).equals(password)) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            return false;
+        }
+        return false;
     }
+
     public void removeItem(String name, double price) {
-          
+        try {
+            Connection c = getCon();
+            PreparedStatement prepareStatement = c.prepareStatement("delete from items where name=" + name + " and price=" + price + ";");
+            prepareStatement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
